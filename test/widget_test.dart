032b9b36e6229c10835f53897e9c33668dd94989
denny200897 <dennyjick@gmail.com>
2025-04-08 23:09:24 +0800

    0

---
+++ test/widget_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:bus/main.dart'; // 使用你的專案名稱
+
+void main() {
+  testWidgets('NotebookApp smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(NotebookApp());
+
+    // Verify that our app starts with the initial state.
+    expect(find.text('還沒有筆記，快來添加吧！'), findsOneWidget);
+  });
+}
